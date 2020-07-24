0455a47ea67c3d509d08f25968825fe9b5fb6f8a
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-07-24 11:57:43 +0300

    add routing to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AppComponent } from './core/app.component';
+
 
 @NgModule({
   declarations: [
