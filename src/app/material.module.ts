f6f3d974cde5d2c44a27c47bde584f2b6a7db260
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-07-26 17:54:29 +0300

    finish footer styling and links to social media

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,7 @@ import {MatRadioModule} from '@angular/material/radio';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInput, MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -12,13 +13,15 @@ import {MatButtonModule} from '@angular/material/button';
     MatRadioModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     MatRadioModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
 
 })
